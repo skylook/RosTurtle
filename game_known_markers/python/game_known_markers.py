eec33fddd74114e0b10f43c341d5a1f747bbf867
Denow <johan@denow.nl>
2017-03-15 12:15:25 +0100

    Update game_known_markers.py

--- game_known_markers/python/game_known_markers.py
+++ game_known_markers/python/game_known_markers.py
@@ -16,7 +16,7 @@ from geometry_msgs.msg import Point
 
 #Known markers in the map (0 - 11)
 #MARKERS_IN_ARENA = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-MARKERS_IN_ARENA = [0, 1, 9]
+MARKERS_IN_ARENA = [0, 5, 11]
 #MARKER_SORT = [0, 2, 3, 4, 5, 6, 7, 8, 9, 1, 11]
 MARKER_SORT = MARKERS_IN_ARENA
 
@@ -28,7 +28,7 @@ class GamePlan(object):
     This gameplan assumes the (wall)marker positions are already known and saved
     '''
 
-    def __init__(self, debug_logging=False):
+    def __init__(self, arena_markers, arena_markers_order, marker_file_location="", marker_file_name="markers", debug_logging=False):
         #Constructor
 
         #if debug logging is True then perform prints to console
@@ -38,15 +38,21 @@ class GamePlan(object):
         self._console_log("GamePlan started")
 
         #vars
-        self._detect_markers = DetectMarkers(False)
+        self._detect_markers = DetectMarkers(marker_file_location, marker_file_name, False, False)
+        #Load markers
+        self._detect_markers.load_from_file(marker_file_name)
+
+        #vars
         self._markers_reached = []
         self._marker_pos_time = 5.0
+        self._arena_markers = arena_markers
+        self._arena_markers_order = arena_markers_order
 
     def play_game(self):
         #Play the game!
 
         #Iterate over the known markers in the defined order
-        for marker_id in MARKER_SORT:
+        for marker_id in self._arena_markers_order:
             self._console_log("ar_marker_" + str(marker_id))
 
             self.move_to_marker(marker_id)
@@ -62,9 +68,9 @@ class GamePlan(object):
         ac = actionlib.SimpleActionClient("move_base", MoveBaseAction)
 
         #wait for the action server to come up
-        while(not ac.wait_for_server(rospy.Duration.from_sec(5.0))):
-                rospy.loginfo("Waiting for the move_base action server to come up")
-        
+        while not ac.wait_for_server(rospy.Duration.from_sec(5.0)):
+            rospy.loginfo("Waiting for the move_base action server to come up")
+
         #Get marker information
         marker_name = "ar_marker_" + str(marker_id)
         marker = self._detect_markers.get_markers()[marker_name]
@@ -81,7 +87,7 @@ class GamePlan(object):
 
         # moving towards the goal*/
 
-        goal.target_pose.pose.position =  Point(goal_x,goal_y,0)
+        goal.target_pose.pose.position = Point(goal_x, goal_y, 0)
 
         #Transform orientation
         if marker['derived']:
@@ -96,17 +102,13 @@ class GamePlan(object):
         rospy.loginfo("Sending goal location ...")
         ac.send_goal(goal)
 
-        ac.wait_for_result(rospy.Duration(60))
+        ac_state = None
+        while ac_state != GoalStatus.SUCCEEDED:
+            ac.wait_for_result(rospy.Duration(60))
+            ac_state = ac.get_state()
+            if ac_state != GoalStatus.SUCCEEDED:
+                ac.send_goal(goal)
 
-        if(ac.get_state() ==  GoalStatus.SUCCEEDED):
-            self._console_log("You have reached the destination")
-            self._markers_reached.append(marker_name)
-            return True
-
-        else:
-            self._console_log("The robot failed to reach the destination")
-            return False
-    
     def _console_log(self, message):
         if self.debug:
             #rospy.loginfo(message)
@@ -120,12 +122,17 @@ if __name__ == '__main__':
     rospy.init_node(ros_node_name)
     print("Node started: " + ros_node_name)
 
-    game_plan = GamePlan(False)
+    game_plan = GamePlan(MARKERS_IN_ARENA, MARKER_SORT)
 
-    input_var = raw_input("Enter r to run, anything else to cancel")
-    if input_var == "r":
+
+    tmp_input = raw_input("Press Enter to move to the starting position")
+    game_plan.move_to_marker(0)
+
+
+    input_var = raw_input("Press Enter to run, Enter anything else (eg. Q) to cancel")
+    if not input_var:
         #Run game
         game_plan.play_game()
-    
+        print("Klaaaaaaaaar!!!!!!!!!!!")
 
     rospy.spin()
